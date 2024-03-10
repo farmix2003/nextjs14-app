c91acaec8dfa9d37395187c1375fb34fa259ff66
Farmix2003 <ffarrux386@gmail.com>
2024-03-10 11:04:05 +0200

    Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import styles from "./footer.module.css";
+import Image from "next/image";
+import { logo } from "../../../public";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <Image className={styles.img} src={logo} alt="footer-logo" />
+      </div>
+      <div className={styles.text}>
+        Farmix Creative Thoughts agency © All Rights Reserved
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
