2f23e4423aa7579299569a43ccbd74b438a22724
Farmix2003 <ffarrux386@gmail.com>
2024-03-10 14:04:15 +0200

    About Page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,9 +3,35 @@ import React from "react";
 import styles from "./about.module.css";
 const About = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h3 className={styles.subtitle}>About Agency</h3>
+        <h2 className={styles.title}>
+          We create digital ideas which are bigger, bolder, braver and better
+        </h2>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime illo,
+          corporis tempora quisquam libero voluptatum, similique adipisci quas
+          eligendi dignissimos dolore eos. Modi reiciendis laudantium odit ea
+          cupiditate distinctio fugiat?
+        </p>
+        <div className={styles.stats}>
+          <div className={styles.stat}>
+            <h1>10K+</h1>
+            <p>Year of Experience</p>
+          </div>
+          <div className={styles.stat}>
+            <h1>10K+</h1>
+            <p>Year of Experience</p>
+          </div>
+          <div className={styles.stat}>
+            <h1>10K+</h1>
+            <p>Year of Experience</p>
+          </div>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
-        <Image src="/about.png" alt="about" fill />
+        <Image className={styles.img} src="/about.png" fill alt="about" />
       </div>
     </div>
   );
