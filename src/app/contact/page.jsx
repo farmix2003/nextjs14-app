5b7a95baf4d3072944a9075a64bd96d5c9a9184b
Farmix2003 <ffarrux386@gmail.com>
2024-03-12 12:32:09 +0200

    Contact

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,29 @@
 import React from "react";
-
+import styles from "./contact.module.css";
+import Image from "next/image";
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/contact.png" alt="contact" fill />
+      </div>
+      <div className={styles.formContainer}>
+        <form className={styles.form}>
+          <input type="text" placeholder="Full Name" />
+          <input type="email" placeholder="Email" />
+          <input type="text" placeholder="Phone Number (Optional)" />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Message..."
+          ></textarea>
+          <button> Send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
